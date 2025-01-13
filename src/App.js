91b3601be1f1d81b82b1795b88ee47926249d4fe
Gökhan <gokhansisman@outlook.com>
2025-01-14 02:58:15 +0300

    Header part completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div>
+    <div className="main-container">
       <header>
-       <h1>Hello Hackers!</h1>
+        <nav className="nav-bar">
+          <div aria-label="title" className="title">
+            <a href="/" className="nav-item">
+              hackernews.dev
+            </a>
+          </div>
+          <div className="nav-list">
+            <a href="/categories" className="nav-item">
+              categories
+            </a>
+            <a href="/articles" className="nav-item">
+              articles
+            </a>
+            <a href="/about" className="nav-item">
+              about
+            </a>
+          </div>
+        </nav>
+        <div className="header-welcome">
+          <div className="header-logo">
+            <img src={logo} className="logo" alt="logo" />
+          </div>
+          <div className="header-text">
+            <h1>Writings for Hackers</h1>
+            <h2>Curated collection of articles for busy developers</h2>
+          </div>
+        </div>
       </header>
-
     </div>
   );
 }
